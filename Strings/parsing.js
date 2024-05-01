a44bd0c7b939d89c2605698f9e7cbb4fa7ba2e68
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-01 14:02:39 -0400

    created parsing and part code labs

---
+++ Strings/parsing.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let name = "Brenda Kaye";
+let firstName;
+let lastName;
+let namePose
+
+
+
+function parseAndDisplayName (name){
+    namePose = name.indexOf(" ");
+    firstName = name.substring(0, namePose);
+    lastName = name.substring(1 + namePose);
+    console.log("Name: " + name);
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
+    // console.log(namePose);
+
+}
+parseAndDisplayName("bob steven");
+
