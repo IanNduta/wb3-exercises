5990273f11ae786dec1899093754c99b5bef0dfe
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-02 12:34:15 -0400

    Replace lab

---
+++ Strings/replace_strings.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let message = "our corporate offices are located in Dallas";
+let newMessage = message.replace("Dallas", "Austin");
+//newMessage.match(/Dallas/gi);
+console.log(newMessage);
